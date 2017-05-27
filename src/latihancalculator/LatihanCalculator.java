568f473c2dc6f4f4878ba4ef91b59a287ac8fe5e
ariefansori <arief.ansori@gmail.com>
2017-05-28 01:34:38 +0700

    Add files via upload

---
+++ src/latihancalculator/LatihanCalculator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihancalculator;
+
+/**
+ *
+ * @author NEPU
+ */
+public class LatihanCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FormCalculator().setVisible(true);
+    }
+    
+}
